0ff2afd6f648228799de808a6d119961f06cb7b3
KevTav11 <ceinnova08578@innovaccion.mx>
2022-06-02 15:06:23 -0500

    Initial commit

---
+++ Hola mundo.py
@@ -0,0 +1,4 @@
+#Este codigo pregunta el nombre de usuario y lo saluda
+nombre = input("Como te llamas? ")
+
+print(f"Hola, {nombre}")#la f es para concatenar
---
+++ Tipos de datos.py
@@ -0,0 +1,26 @@
+#String - cadena de texto
+from pickle import TRUE
+
+nombre = "Kevin"
+
+#Numeros enteros 
+edad =18
+edads="18"
+
+#diferencia
+print(edad+edad)
+print(edads+edads)
+
+#Float - numero con punto decimal
+
+edad = float(edads) #Cesteo - Transformacion un tipo de variable a otro 
+
+#Usa type() para saber de que tipo es la variable
+print(type(nombre), type(edad))
+
+#Bool - Booleano - Si o No
+
+legustas = True
+legustas = False
+
+print(legustas)
